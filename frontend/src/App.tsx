6bd7f20fdb89e2213dedb78d92ecac524adff504
cyeongb <cyeongb@gmail.com>
2025-07-04 09:58:39 +0900

    commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Header from './components/Layout/Header'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <LoginPage />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
